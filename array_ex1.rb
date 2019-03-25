210425215ca8a2f63f66e6c364ae0871d96fa605
Tom Nagle <tom.nagle22@gmail.com>
2019-03-25 13:00:19 -0400

    Array chapter exercises

---
+++ array_ex1.rb
@@ -0,0 +1,5 @@
+#ex 1: Check for the number
+
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+arr.each { |x| puts "yes" if x == number }
---
+++ array_ex7.rb
@@ -0,0 +1,5 @@
+arr = [2, 4, 6, 8]
+arr_new = []
+arr.each { |x| arr_new << x + 2 }
+p arr
+p arr_new
